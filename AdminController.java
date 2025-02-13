796310e7b58d5e9034a34857fd3e41f4bbdd873f
Krishnavel007 <krishnavel.m2807@gmail.com>
2025-02-13 23:52:15 +0530

    Add files via upload

---
+++ AdminController.java
@@ -0,0 +1,32 @@
+package com.ifet.adminservice.controller;
+import com.ifet.adminservice.model.Product;
+import com.ifet.adminservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/products")
+
+public class AdminController {
+	
+	@Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+
+}
